120c564ee76943c02f42f3f26c10ecbb47785cfb
Matheus Oliveira Silva <matheussilva@tecgraf.puc-rio.br>
2025-03-04 12:10:48 -0300

    simple rag prompt setup

---
+++ agent/prompts/prompts.py
@@ -0,0 +1,8 @@
+SIMPLE_RAG_PROMPT = """
+You are an AI Engineer specialist. User will ask you questions about AI Engineering and you need to answer based on the context retrieved.
+
+Context:
+{relevant_documents}
+
+You can ask for clarification and say you don't know the answer if you don't have enought context to answer it.
+"""
