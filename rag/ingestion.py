6efa9a50adac0d853b40c1b9697a3023b8551cc4
Matheus Oliveira Silva <matheussilva@tecgraf.puc-rio.br>
2025-03-05 14:05:19 -0300

    update: ingestion pipeline to include preprocessment (contextual embeddings)

--- rag/ingestion.py
+++ rag/ingestion.py
@@ -1,31 +1,48 @@
 from rag.chunking.chunking_config import ChunkingConfig
 from vector_store.pinecone_utils import PineconeUtils
 from langchain_community.document_loaders import DirectoryLoader
+from rag.preprocessing.preprocessing import Preprocesser
 from pathlib import Path
 
 class Ingestion:
     def __init__(self,
-                 chunk_size, chunk_overlap, chunking_strategy,
+                 index_name,
+                 chunking_strategy,
                  directory, embedding_model_name, metric,
+                 preprocessing_technique: str = "None",
                  model_provider: str = 'openai',
                  glob: str = '*.pdf'):
 
-        self.chunk_size = chunk_size
-        self.chunk_overlap = chunk_overlap
+        self.chunk_size = 512
+        self.chunk_overlap = 50
         self.chunking_strategy = chunking_strategy
         self.chunker = self.define_chunker()
 
+        print("[Ingestion pipeline] Loading documents...")
         self.docs = self.load_documents(directory, glob)
+
+        print("[Ingestion pipeline] Chunking documents...")
         self.chunked_docs = self.chunker.split_documents(self.docs)
 
+        if preprocessing_technique != "None":
+            print(f"[Ingestion pipeline] Preprocessing documents (with {preprocessing_technique} strategy)...")
+            self.chunked_docs = self.preprocess_documents(self.chunked_docs, preprocessing_technique)
+
+        print("[Ingestion pipeline] Sending documents into Pinecone...")
         self.pinecone_utils = PineconeUtils(
-            index_name=f'rag-{embedding_model_name}-{metric}',
+            index_name=index_name,
             metric=metric,
             embedding_model_name=embedding_model_name,
             embedding_provider=model_provider
         )
 
         self.pinecone_utils.insert_documents(self.chunked_docs)
+        print("[Ingestion pipeline] Done!")
+
+    @staticmethod
+    def preprocess_documents(docs, preprocessing_technique):
+        preprocessor = Preprocesser(preprocessing_technique)
+        return preprocessor.preprocess_documents(docs)
 
     def define_chunker(self):
         chunking_config = ChunkingConfig(
@@ -49,10 +66,10 @@ if __name__ == "__main__":
     path = ROOT_DIR / 'documents'
 
     ingestion = Ingestion(
-        chunk_size=512,
-        chunk_overlap=50,
-        chunking_strategy='fixed-size-chunking',
+        index_name="ca-contextualemb-dotp-3large",
+        chunking_strategy='content-aware',
         metric='dotproduct',
         directory=path,
-        embedding_model_name='text-embedding-3-large'
+        embedding_model_name='text-embedding-3-large',
+        preprocessing_technique='contextual-embedding'
     )
